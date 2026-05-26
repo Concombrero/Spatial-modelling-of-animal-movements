15c4128a707b500ffd98848ae3031c9654588a52
Timothé Boyer <boyer.tim.travail@gmail.com>
2026-05-26 12:02:01 +0200

    FEAT First Structure Simulation

---
+++ script/Simulation_Day_Night/simulation.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+class Config:
+    """Class to hold the configuration for the simulation"""
+    
+    def __init__(self):
+        pass
+
+
+class Simulation:
+    """Class to run the simulation day and night based with smell and sight detection"""
+    
+    def __init__(self):
+        pass
+
+
+class SimulationResult:
+    """Class to hold and plot the results of the simulation"""
+    
+    def __init__(self):
+        pass
